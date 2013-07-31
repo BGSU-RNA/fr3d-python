701234b75eee650a683a0712db9c387a4e707f26
Blake Sweeney <blakes.85@gmail.com>
2013-07-31 15:31:56 -0400

    Reorganize tests

--- tests/superpositions_test.py
+++ tests/superpositions_test.py
@@ -1,10 +1,15 @@
+from unittest import TestCase
 
 from numpy import array
-from fr3d.geometry.superpositions import bestrotation
+from numpy.testing import assert_almost_equal
 
-a = array([(1,0,1),(0,1,0),(0,0,1)])
-
-rotation = bestrotation(a,a)
+#from fr3d.geometry.superpositions import bestrotation
 
 
+class BestRotationTest(TestCase):
 
+    def test_list_almost_equals(self):
+        val = array([[1.0, 2.0, 3.0], [4.0, 4.1, 5.0]])
+        ans = array([[1.0, 2.0, 3.0],
+                     [3.999999999999999999999999999, 4.1, 5.0]])
+        assert_almost_equal(val, ans)
