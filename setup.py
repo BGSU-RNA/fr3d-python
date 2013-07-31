7ee096d09e29b74ca7212f5ac78e4643a295ec54
Blake Sweeney <blakes.85@gmail.com>
2013-07-31 13:52:48 -0400

    Setup FR3D as a python package

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='fr3d',
+    version='0.0.1',
+    packages=['fr3d'],
+    url='',
+    license='LICENSE.txt',
+    description='Python implementation of FR3D',
+    long_description="""
+    """
+)
