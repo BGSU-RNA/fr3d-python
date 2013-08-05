d300e980ec5cb14513ce477f3a248dfbd6a00dd3
Blake Sweeney <blakes.85@gmail.com>
2013-08-05 17:44:30 -0400

    Add test for FR3D cif reader
    The FR3D cif reader will be a wrapper around the PDBx CIF reader to provide the
    useful abstractions and functions we need. One such function is computing unit
    ids for all entries in a CIF file.

--- tests/cif_test.py
+++ tests/cif_test.py
@@ -2,6 +2,8 @@ from unittest import TestCase
 
 from pdbx.reader.PdbxParser import PdbxReader as Reader
 
+from fr3d.cif import FR3DReader
+
 
 class CifTest(TestCase):
     def setUp(self):
@@ -13,3 +15,14 @@ class CifTest(TestCase):
 
     def test_loads_a_file(self):
         self.assertIsNot(self.data, [])
+
+
+class FR3DReaderTest(TestCase):
+    def setUp(self):
+        with open('files/1GID.cif', 'rb') as raw:
+            self.reader = FR3DReader(raw)
+
+    def test_generates_all_unit_ids(self):
+        val = len(self.reader.unit_ids())
+        ans = 350
+        self.assertEqual(val, ans)
