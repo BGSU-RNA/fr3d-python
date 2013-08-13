e6aab45764aa94c28156e46d84f47fb2343b5c23
Craig L. Zirbel <zirbel@bgsu.edu>
2013-08-13 14:46:30 -0400

    Superposition test made
    but probably won't run right

--- tests/superpositions_test.py
+++ tests/superpositions_test.py
@@ -3,8 +3,8 @@ from unittest import TestCase
 from numpy import array
 from numpy.testing import assert_almost_equal
 
-#from fr3d.geometry.superpositions import bestrotation
-
+from fr3d.geometry.superpositions import bestrotation
+from fr3d.geometry.superpositions import besttransformation
 
 class BestRotationTest(TestCase):
 
@@ -13,3 +13,11 @@ class BestRotationTest(TestCase):
         ans = array([[1.0, 2.0, 3.0],
                      [3.999999999999999999999999999, 4.1, 5.0]])
         assert_almost_equal(val, ans)
+
+class BestRotationTest(TestCase):
+    def test_list_almost_equals(self):
+		a = array([[ 1.0000, 0.0000, 0.0000],[ 0.0000, 1.0000, 0.0000],[ 0.0000, 0.0000, 1.0000]])
+		b = array([[ 0.0000, 1.0000, 0.0000],[-1.0000, 0.0000, 0.0000],[ 0.0000, 0.0000, 1.0000]])
+		rot = array([[-0.0000,-1.0000, 0.0000],[ 1.0000, 0.0000, 0.0000],[ 0.0000, 0.0000, 1.0000]])
+		rotation, shift = besttransformation(a,b)
+        assert_almost_equal(rot,rotation)
