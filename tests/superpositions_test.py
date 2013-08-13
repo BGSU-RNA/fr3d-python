97b350dde0269dce8dafedbdd09232f01c9bf994
Craig L. Zirbel <zirbel@bgsu.edu>
2013-08-13 14:51:22 -0400

    Rotation and Transformation tests

--- tests/superpositions_test.py
+++ tests/superpositions_test.py
@@ -7,17 +7,26 @@ from fr3d.geometry.superpositions import bestrotation
 from fr3d.geometry.superpositions import besttransformation
 
 class BestRotationTest(TestCase):
-
     def test_list_almost_equals(self):
-        val = array([[1.0, 2.0, 3.0], [4.0, 4.1, 5.0]])
-        ans = array([[1.0, 2.0, 3.0],
-                     [3.999999999999999999999999999, 4.1, 5.0]])
-        assert_almost_equal(val, ans)
+        a = array([[ 1.0000, 0.0000, 0.0000],[ 0.0000, 1.0000, 0.0000],[ 0.0000, 0.0000, 1.0000],[-1.0000, 0.0000, 0.0000],[ 0.0000,-1.0000, 0.0000],[ 0.0000, 0.0000,-1.0000]])
+        b = array([[ 0.0000, 1.0000, 0.0000],[-1.0000, 0.0000, 0.0000],[ 0.0000, 0.0000, 1.0000],[ 0.0000,-1.0000, 0.0000],[ 1.0000, 0.0000, 0.0000],[ 0.0000, 0.0000,-1.0000]])
+        rot = array([[ 0.0000,-1.0000, 0.0000],[ 1.0000, 0.0000, 0.0000],[ 0.0000, 0.0000, 1.0000]])
+        rotation, shift = besttransformation(a,b)
+        assert_almost_equal(rot,rotation)
 
-class BestRotationTest(TestCase):
+
+class BestTransformationTest(TestCase):
     def test_list_almost_equals(self):
 		a = array([[ 1.0000, 0.0000, 0.0000],[ 0.0000, 1.0000, 0.0000],[ 0.0000, 0.0000, 1.0000]])
 		b = array([[ 0.0000, 1.0000, 0.0000],[-1.0000, 0.0000, 0.0000],[ 0.0000, 0.0000, 1.0000]])
 		rot = array([[-0.0000,-1.0000, 0.0000],[ 1.0000, 0.0000, 0.0000],[ 0.0000, 0.0000, 1.0000]])
 		rotation, shift = besttransformation(a,b)
         assert_almost_equal(rot,rotation)
+
+class BestRotationTest(TestCase):
+
+    def test_list_almost_equals(self):
+        val = array([[1.0, 2.0, 3.0], [4.0, 4.1, 5.0]])
+        ans = array([[1.0, 2.0, 3.0],
+                     [3.999999999999999999999999999, 4.1, 5.0]])
+        assert_almost_equal(val, ans)
