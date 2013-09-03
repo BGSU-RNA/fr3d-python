ef01a6bdb2846c7bad73112f6e0b99ff028e1f5f
Kevin Stoll <kevin.e.stoll@gmail.com>
2013-09-03 16:09:28 -0400

    Code changes to follow the steps from wiki Kabsch
    http://en.wikipedia.org/wiki/Kabsch_algorithm

--- fr3d/geometry/superpositions.py
+++ fr3d/geometry/superpositions.py
@@ -8,14 +8,13 @@ import numpy
 def besttransformation(set1, set2):
     """This finds the 3x3 rotation matrix which optimally superimposes
     the nx3 matrix of points a onto the nx3 matrix of points b.
-    It is assumed that the coordinates of a and b have mean 0.
     One reference is this: http://en.wikipedia.org/wiki/Kabsch_algorithm
     Another is a python implementation that goes with pymol, see
     http://www.pymolwiki.org/index.php/Kabsch
 
-    The arguments should be lists or numpy arrays of the form:
-    set1=[[1.0, 2.0, 3.0], [4,5,2], [9,2,4]]
-    sel2=[[4.0, 4.1, 5.0], [2,3,1], [3,1,4]]
+    The arguments should be lists or numpy arrays of the form exampled below:
+    set1=[[1.0, 2.0, 3.0], [4,5,2], [9,2,4], ....,[3,1,6]]
+    sel2=[[4.0, 4.1, 5.0], [2,3,1], [3,1,4], ....,[1, 7, 7]]
 
     :set1: A list or a numpy array of (n, 3) coordinates.
     :set2: A list or a numpy array of (n, 3) coordinates.
@@ -23,61 +22,66 @@ def besttransformation(set1, set2):
     set of coordinates, respectively.
     """
 
-    # Check to make sure same number of (x,y,z) coordinates in each set.
+    # Check to make sure same number of (x,y,z) coordinates both sets.
     #If condition is not true this program stops.
     assert len(set1) == len(set2)
     length = len(set1)
     assert length > 0
 
-    #These add all x_{ij}'s, y_{ij}'s, z_{ij}'s for each element in sel_j j=1,2
+    # Translation Step is beginning.
+    # These add all x_{ij}'s, y_{ij}'s, z_{ij}'s for each element in 
+    # setj j=1,2
     # i=1,2,3,..length
     # /float(length) divides by length
-    #This creates a [mean x_j, mean y_j, mean z_j] for all set of coordinates j
-    COM1 = numpy.sum(set1, axis=0) / float(length)
-    COM2 = numpy.sum(set2, axis=0) / float(length)
+    # This creates a [mean x_j, mean y_j, mean z_j] for both sets of 
+    # coordinates setj j=1, or 2, or the centroid of both sets.
+    mean1 = numpy.sum(set1, axis=0) / float(length)
+    mean2 = numpy.sum(set2, axis=0) / float(length)
 
-    #individual deviation from group means.
-    #Specifically,
-    #subtract x_{ij} by the mean x_j's. Here i=1,2,..length.  Same for y, and z
-    dev1 = set1 - COM1
-    dev2 = set2 - COM2
+    # Next,
+    # Subtract x_{ij} by the mean of x_j's. Here i=1,2,..length.  
+    # Same for y, and z.
+    dev1 = set1 - mean1
+    dev2 = set2 - mean2
+    #  Thus, both sets are translated, so that their centroid coincides with 
+    # the origin of the coordinate system.
+    # Translation Step is now completed.
 
-    # This is only used in RMSD computation which is not used anywhere.
-    #[sum(x_{i1}^2) + sum(x_{i2}^2) +
-    # sum(y_{i1}^2) + sum(y_{i2}^2) +
-    # sum(z_{i1}^2) + sum(z_{i2}^2)]
-    E0 = numpy.sum(numpy.sum(dev1 * dev1, axis=0), axis=0) + \
-        numpy.sum(numpy.sum(dev2 * dev2, axis=0), axis=0)
+    
+    # Begin Step to Compute the 3X3 Covariance Matrix, A.
+    A=numpy.dot(numpy.transpose(dev2), dev1)
+    # Covariance Matrix, A, is now calculated
 
-#********* I DO NOT UNDERSTAND STARTING HERE TO......************
-    # V and Wt are the orthonormal bases that when multiplied by each other
-    # give us the rotation matrix, U.  S, (Sigma, from SVD) provides us with
-    # the error.
-    V, S, Wt = numpy.linalg.svd(numpy.dot(numpy.transpose(dev2), dev1))
 
-    # we already have our solution, in the results from SVD.
+    # Begin of the Computation of the optimal rotation matrix using 
+    # Singular Value Decomposition (SVD) 
+    V, diagS, Wt = numpy.linalg.svd(A)
+    # V and Wt are 3x3 orthonormal bases, diagS is the diagonal elements of
+    # a 3x3 diagonal matrix, S, in regular SVD.  In SVD, recall that the 
+    # Covariance matrix, A, is A=V*S*transpose(W) (matrix multiplication).
+    #S=numpy.diag(diagS)
+    #A=numpy.dot(V,numpy.dot(S,Wt)
+
+    
+    # The next step is to decide whether we need to correct our rotation 
+    # matrix to ensure a right-handed coordinate system
     # we just need to check for reflections and then produce
     # the rotation.  V and Wt are orthonormal, so their det's
     # are +/-1.
-    reflect = numpy.linalg.det(V) * numpy.linalg.det(Wt)
+    I=numpy.matrix(numpy.identity(3))
+    d=numpy.linalg.det(numpy.dot(numpy.transpose(Wt),numpy.transpose(V)))
+    if numpy.isclose(d, -1.0):
+        I[2,2]=d
 
-    if numpy.isclose(reflect, -1.0):
-        S[-1] = -S[-1]
-        V[:, -1] = -V[:, -1]
-
-    # This does appear to be used anywhere, what does it do?
-    RMSD = E0 - (2.0 * sum(S))
-    RMSD = numpy.sqrt(abs(RMSD / length))
-
-#*********....HERE.************
+    # End of the Computation of the optimal rotation matrix
 
     #The transformation matrix, U, is now V*Wt
-    U = numpy.dot(V, Wt)
+    U = numpy.dot(numpy.dot(numpy.transpose(Wt), I),numpy.transpose(V))
 
     # rotate and translate the molecule
-    #sel2 = numpy.dot((mol2 - COM2), U)
+    #sel2 = numpy.dot((set2 - Mean2), U)
     new1 = numpy.dot(dev1, U)
-    new2 = numpy.dot(dev2, U)
+    new2 = dev2
 
     #Return the transformation matrix, the new coordinates for the two
     #set of coordinates, respectively.
