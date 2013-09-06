48809965ec2cff3b68d4652a9ce3e8e2ca202422
Blake Sweeney <blakes.85@gmail.com>
2013-09-06 14:18:03 -0400

    Basic reader implementation
    This reader does not compute the symmetry operations needed to generate all
    possible atoms because it assumes all operations are 1_555. I will fix this up
    later, but for now this works as a starting point, I think.

--- fr3d/cif/reader.py
+++ fr3d/cif/reader.py
@@ -1,3 +1,5 @@
+import re
+
 from collections import defaultdict
 
 import numpy as np
@@ -33,16 +35,22 @@ class CIF(object):
     def __residues__(self, entry):
         mapping = defaultdict(list)
         for atom in self.__atoms__(entry):
-            mapping[atom.component_id()].append(atom)
+            mapping[atom.component_unit_id()].append(atom)
 
         residues = []
         for comp_id, atoms in mapping.items():
             # TODO: Set residue data
-            residues.append(Component({}, atoms))
+            first = atoms[0]
+            residues.append(Component({
+                'sequence': first.component_id,
+                'number': first.component_number,
+                'ins_code': first.ins_code,
+            }, atoms))
         return residues
 
     def __atoms__(self, entry):
-        for atom in entry.getObj('_atom_site'):
+
+        for atom in self.__table__(entry, 'atom_site'):
             for symmetry in self.__find_symmetries__(entry, atom):
                 if not symmetry:
                     raise InvalidSymmetry
@@ -50,15 +58,22 @@ class CIF(object):
                 x, y, z = self.__apply_symmetry__(atom, symmetry)
                 # TODO: Set up atom data
                 yield Atom({
+                    'pdb': entry.getName(),
+                    'model': atom['label_entity_id'],
+                    'chain': atom['label_asym_id'],
+                    'component_id': atom['label_comp_id'],
+                    'component_number': atom['label_seq_id'],
+                    'ins_code': atom['pdbx_PDB_ins_code'],
                     'x': x,
                     'y': y,
                     'z': z,
+                    'name': atom['label_atom_id'],
                     'symmetry': symmetry['name']
                 })
 
     def __apply_symmetry__(self, atom, symmetry):
         # TODO: Apply the symmetries
-        pass
+        return atom['Cartn_x'], atom['Cartn_y'], atom['Cartn_z']
         #coords = np.array([atom['x'], atom['y'], atom['z']])
         #transformed = np.mul(coords, symmetry['matrix'])
         #return transformed[0], transformed[1], transformed[2]
@@ -82,5 +97,17 @@ class CIF(object):
             }
 
         # TODO: Find the symmetries
+        return [{
+            'name': '1_555',
+            'translate': 0,
+            'rotate': np.array([[1.0, 0.0, 0.0],
+                                [0.0, 1.0, 0.0],
+                                [0.0, 0.0, 1.0]])
+        }]
 
-        return None
+    def __table__(self, entry, name):
+        block = entry.getObj(name)
+        columns = block.getItemNameList()
+        columns = [re.sub('_.+\.', '', column) for column in columns]
+        for index in xrange(block.getRowCount()):
+            yield dict(zip(columns, block.getRow(index)))
