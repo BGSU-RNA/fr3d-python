e33f309e298b9a0940e440b06dc0fc27b23f0b02
Kevin Stoll <kevin.e.stoll@gmail.com>
2013-09-26 18:32:00 -0400

    Program compute RMSD

---
+++ fr3d/geometry/RMSD.py
@@ -0,0 +1,14 @@
+import numpy
+
+def RMSD(set1, set2):
+    """This function calculates the root mean square difference of the 
+    atomic positions of set1 with set2.  In other words, this function
+    calculates the root mean squared differences between two sets of a
+    n 3-dimensional coordinates.  The calculation is explained here:
+    wikipedia.org/wiki/Root-mean-square_deviation_of_atomic_positions
+    """
+    assert len(set1) == len(set2)
+    L = len(set1)
+    assert L > 0
+    distance=numpy.sqrt(numpy.sum(numpy.power(set1 - set2,2))/L)
+    return distance
