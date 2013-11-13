7f4bc465d10c5baa5b7c51d55cba330259a79c4a
Blake Sweeney <blakes.85@gmail.com>
2013-11-13 12:08:28 -0500

    Clean up and add an exception for missing centers
    I added some exception throwing and the like to discrepancy. We now fail if an
    unknown center is requested. In addition, I did some minor code clean ups like
    adding and removing whitespace. I also added some basic documentation to the
    function.

--- fr3d/geometry/discrepancy.py
+++ fr3d/geometry/discrepancy.py
@@ -1,56 +1,76 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Wed Nov 06 17:19:16 2013
 
-@author: zirbel
-"""
+import numpy as np
 
 import fr3d.geometry.angleofrotation
 
-# centers is a list like ['base','P','C1*','ribose']
 
-def discrepancy(ntlist1,ntlist2,centers=['base'],weights=1.0,angleweight=1.0):
-    
-    assert(length(ntlist1)) == length(ntlist2))
+class MissingBaseException(Exception):
+    """An exception that is raised when a base center that does not exist is
+    requested.
+    """
+    pass
+
+
+def discrepancy(ntlist1, ntlist2, centers=['base'], weights=1.0,
+                angleweight=1.0):
+    """Compute the geometric discrepancy between two lists of components.
+
+    :ntlist1: The first list of components.
+    :ntlist2: The second list of components.
+    :centers: A list of center names to use, such as
+        ['base', 'P', 'C1*', 'ribose']
+    :weights: The weights to use. If only one weight is given it is used for
+    all centers.
+    :angleweight: The weighting for angles to use.
+    :returns: The geometric discrepancy.
+    """
+
+    assert length(ntlist1) == length(ntlist2)
+
+    # TODO: Should we allow users to pass a tuple too?
+    if not isinstance(centers, list):
+        centers = [centers]
 
     if not isinstance(weights, list):
         weights = [weights] * len(centers)
 
     R = []
     S = []
-    
-    for i in range(0,length(ntlist1)):
+
+    for i in xrange(len(ntlist1)):
         nt1 = ntlist1[i]
         nt2 = ntlist2[i]
         for c in centers:
+            coords1 = list(nt1.coordinates(name=center))
+            coords2 = list(nt2.coordinates(name=center))
+
             if c in nt1.centers:
                 R.append(nt1.centers[c])
                 S.append(nt2.centers[c])
                 W.append(weights[i])
-            elif c in nt1.atoms:
-                R.append(nt1._atoms[c])
-                S.append(nt2._atoms[c])
+            elif coords1:
+                R.append(coords1[0])
+                S.append(coords2[0])
                 W.append(weights[i])
             else:
-                # raise an exception (or just notify the user?)
-                
+                raise MissingBaseException(center)
+
     # superimpose R and S
-                
-    rotation_matrix, RMSD = superimposeweighted(R,S,W)
-    
+    rotation_matrix, RMSD = superimposeweighted(R, S, W)
+
     # loop through the bases and calculate angles between them
 
     orientationerror = 0
-    
+
     if 'base' in centers:
-        for i in range(0,length(ntlist1)):
+        for i in xrange(len(ntlist1)):
             R1 = ntlist1[i].rotation_matrix
             R2 = ntlist2[i].rotation_matrix
-            
+
             # calculate angle in radians, or adjust with angleweight factor
             angle = angleofrotation(R1 * rotation_matrix * R2')
 
             orientationerror += angle^2
-            
+
     discrepancy = sqrt(RMSD^2 + angleweight*orientationerror) / length(ntlist1)
-    
