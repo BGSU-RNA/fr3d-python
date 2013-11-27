37cb80dd735ad82396e5a07296f2d49d70ef6e63
Kevin Stoll <kevin.e.stoll@gmail.com>
2013-11-27 17:31:38 -0500

    Weighting was not working correctly
    I think this is the most accurate, but still not working.

--- fr3d/geometry/discrepancy.py
+++ fr3d/geometry/discrepancy.py
@@ -5,7 +5,6 @@ from fr3d.geometry.angleofrotation import angle_of_rotation
 from fr3d.geometry.superpositions import besttransformation
 from fr3d.geometry.superpositions import besttransformation_weighted
 
-
 class MissingBaseException(Exception):
     """An exception that is raised when a base center that does not exist is
     requested.
@@ -18,9 +17,21 @@ class MissingPhosphateException(Exception):
     """
     pass
 
-class MissingC1starException(Exception):
-    """An exception that is raised when the C1* atoms are not present in
-    inputted model.
+class LengthofBaseWeightError(Exception):
+    """An exception that is raised when the list of base weights is not equal 
+    in length to the list of nucleotides.
+    """
+    pass
+
+class LengthofPWeightError(Exception):
+    """An exception that is raised when the list of phosphate weights is not 
+    equal in length to the list of nucleotides.
+    """
+    pass
+
+class LengthofC1starWeightError(Exception):
+    """An exception that is raised when the list of C1* weights is not equal in 
+    length to the list of nucleotides.
     """
     pass
 
@@ -32,8 +43,15 @@ def discrepancy(ntlist1, ntlist2, centers=['base'], base_weights=1.0,
     :ntlist2: The second list of components.
     :centers: A list of center names to use, such as
         ['base', 'P', 'C1*', 'ribose']
-    :weights: The weights to use. If only one weight is given it is used for
-    all centers.
+    :base_weights: The base weights to use. If only one weight is given 
+    it is used for all centers.  Otherwise, provide list of base weights with 
+    same length as the length of ntlist1
+    :P_weigths: The phosphate weights to use. If only one weight is given 
+    it is used for all.  Otherwise, provide list of phosphate weights 
+    with same length as the length of ntlist1
+    :C1star_weights: The C1* weights to use. If only one weight is given 
+    it is used for all.  Otherwise, provide list of C1* weights with 
+    same length as the length of ntlist1
     :angleweight: The weighting for angles to use.
     :returns: The geometric discrepancy.
     """
@@ -46,21 +64,21 @@ def discrepancy(ntlist1, ntlist2, centers=['base'], base_weights=1.0,
 
     if not isinstance(base_weights, list):
         base_weights = [base_weights] * len(ntlist1)
-    
-    if len(base_weights) != len(ntlist1):
-        base_weights = np.ones(len(ntlist1))
-    
+
+    if len(base_weights)!= len(ntlist1):
+        raise LengthofBaseWeightError('Weight length does not match # of nucl.')
+        
     if not isinstance(P_weights, list):
         P_weights = [P_weights] * len(ntlist1)
-    
-    if len(P_weights) != len(ntlist1):
-        P_weights = np.ones(len(ntlist1))
 
+    if len(P_weights)!= len(ntlist1):
+        raise LengthofPWeightError('Weight length does not match # of nucl.')
+    
     if not isinstance(C1star_weights, list):
         C1star_weights = [C1star_weights] * len(ntlist1)
-    
-    if len(C1star_weights) != len(ntlist1):
-        C1star_weights = np.ones(len(ntlist1))
+        
+    if len(C1star_weights)!= len(ntlist1):
+        raise LengthofC1starWeightError('Weight length does not match # of nucl.')
 
     R = []
     S = []
@@ -80,20 +98,20 @@ def discrepancy(ntlist1, ntlist2, centers=['base'], base_weights=1.0,
             else:
                 if c=='base':
                     raise MissingBaseException(centers)
-                if c=='P':
-                    if nt1.coordinates(type = 'P')!=[] and nt2.coordinates(type = 'P')!=[]:
-                        R.append(nt1.coordinates(type = 'P'))
-                        S.append(nt2.coordinates(type = 'P'))
-                        W.append(P_weights[i])
-                    else:
-                        raise MissingPhosphateException(centers)
-                if c=='C1*':
-                    if nt1.coordinates(type = 'C1*')!=[] and nt2.coordinates(type = 'C1*')!=[]:
-                        R.append(nt1.coordinates(type = 'C1*'))
-                        S.append(nt2.coordinates(type = 'C1*'))
-                        W.append(C1star_weights[i])
-                    else:
-                        raise MissingC1starException(centers)
+            if c=='P':
+                if nt1.coordinates(type = 'P')!=[]:
+                    R.append(nt1.coordinates(type = 'P')[0])
+                    S.append(nt2.coordinates(type = 'P')[0])
+                    W.append(P_weights[i])
+                else:
+                    raise MissingPhosphateException(centers)
+            if c=='C1*':
+                if nt1.coordinates(type = 'C1*')!=[] and nt2.coordinates(type = 'C1*')!=[]:
+                    R.append(nt1.coordinates(type = 'C1*')[0])
+                    S.append(nt2.coordinates(type = 'C1*')[0])
+                    W.append(C1star_weights[i])
+                else:
+                    raise MissingPhosphateException(centers)
     #rotation_matrix, _, _, RMSD = besttransformation(R, S)
     # Superimpose R and S with weights? I need to make changes.
     rotation_matrix, new1, mean1, RMSD, sse = besttransformation_weighted(R, S, W)
