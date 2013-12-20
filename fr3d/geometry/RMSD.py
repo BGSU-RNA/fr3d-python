f2dc10e772c57ee50e10932733eba93d0cb94a54
Blake Sweeney <blakes.85@gmail.com>
2013-12-20 14:05:57 -0500

    Whitespace clean ups

--- fr3d/geometry/RMSD.py
+++ fr3d/geometry/RMSD.py
@@ -1,7 +1,8 @@
 import numpy
 
+
 def RMSD(set1, set2):
-    """This function calculates the root mean square difference of the 
+    """This function calculates the root mean square difference of the
     atomic positions of set1 with set2.  In other words, this function
     calculates the root mean squared differences between two sets of a
     n 3-dimensional coordinates.  The calculation is explained here:
@@ -10,19 +11,19 @@ def RMSD(set1, set2):
     assert len(set1) == len(set2)
     L = len(set1)
     assert L > 0
-    distance=numpy.sqrt(numpy.sum(numpy.power(set1 - set2,2))/L)
+    distance = numpy.sqrt(numpy.sum(numpy.power(set1 - set2, 2))/L)
     return distance
-    
+
+
 def sumsquarederror(set1, set2):
-    """This function calculates the summed squared difference of the 
+    """This function calculates the summed squared difference of the
     atomic positions of set1 with set2.  In other words, this function
     calculates the root mean squared differences between two sets of a
     n 3-dimensional coordinates.  The calculation is explained here:
     wikipedia.org/wiki/Root-mean-square_deviation_of_atomic_positions
     """
     assert len(set1) == len(set2)
-    L = len(set1)
-    assert L > 0
-    distance=numpy.sum(numpy.power(set1 - set2,2))
-    #Need to fix this for weighting.
+    assert len(set1) > 0
+    distance = numpy.sum(numpy.power(set1 - set2, 2))
+    # TODO: Need to fix this for weighting.
     return distance
--- fr3d/geometry/angleofrotation.py
+++ fr3d/geometry/angleofrotation.py
@@ -1,17 +1,11 @@
 import numpy
 
-def angle_of_rotation(rotation_matrix):  
-                   
-    angle = numpy.arccos((numpy.trace(rotation_matrix)-1)/2.0)
-    
-    return angle
-    
+
+def angle_of_rotation(rotation_matrix):
+    return numpy.arccos((numpy.trace(rotation_matrix) - 1) / 2.0)
+
+
 def axis_of_rotation(rotation_matrix):
     eigenvalues, eigenvector = numpy.linalg.eig(rotation_matrix)
-
-    location = numpy.where(eigenvalues==1)[0]
-    axis = eigenvector[location]
-
-    return axis
-    
-    
+    location = numpy.where(eigenvalues == 1)[0]
+    return eigenvector[location]
