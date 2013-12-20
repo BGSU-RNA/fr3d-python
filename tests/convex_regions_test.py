94095853d85a374b1d8922473b383c0bf2e2e746
Blake Sweeney <blakes.85@gmail.com>
2013-12-20 13:39:15 -0500

    Change how we import to avoid running bad tests
    If we import something that starts with test then we run it as a test. This is a
    mistake so I've changed how we import so that this does not occur. We how import
    the whole module with a short name and then access the functions that way.

--- tests/convex_regions_test.py
+++ tests/convex_regions_test.py
@@ -1,48 +1,46 @@
 import unittest
 
-from fr3d.geometry.convex_regions import totheleft
-from fr3d.geometry.convex_regions import counterclockwiseinside
-from fr3d.geometry.convex_regions import testcounterclockwiseconvex
+from fr3d.geometry import convex_regions as cr
 
 
 class ToTheLeftTest(unittest.TestCase):
     def test_to_left_matches_left(self):
-        val = totheleft([0, 1], [1, 0])
+        val = cr.totheleft([0, 1], [1, 0])
         self.assertTrue(val)
 
     def test_to_left_rejects_right(self):
-        val = totheleft([1, 0], [0, 1])
+        val = cr.totheleft([1, 0], [0, 1])
         self.assertFalse(val)
 
     def test_to_left_matches_on_line(self):
-        val = totheleft([0, 1], [0, 1])
+        val = cr.totheleft([0, 1], [0, 1])
         self.assertTrue(val)
 
 
 class CounterClockWiseConvexTest(unittest.TestCase):
     def test_matches_counter_clockwise(self):
-        val = testcounterclockwiseconvex([[0, 0], [2, 0], [3, 1], [2, 2],
-                                          [0, 2]])
+        val = cr.testcounterclockwiseconvex([[0, 0], [2, 0], [3, 1], [2, 2],
+                                             [0, 2]])
         self.assertTrue(val)
 
     def test_rejects_clockwise(self):
-        val = testcounterclockwiseconvex([[0, 0], [2, 0], [3, 1], [1, 1],
-                                          [2, 2], [0, 2]])
+        val = cr.testcounterclockwiseconvex([[0, 0], [2, 0], [3, 1], [1, 1],
+                                             [2, 2], [0, 2]])
         self.assertFalse(val)
 
     def test_rejects_another_clockwise(self):
-        val = testcounterclockwiseconvex([[0, 0], [-1, -1], [2, 1], [3, 1],
-                                          [2, 2], [0, 2]])
+        val = cr.testcounterclockwiseconvex([[0, 0], [-1, -1], [2, 1], [3, 1],
+                                             [2, 2], [0, 2]])
         self.assertFalse(val)
 
 
 class CounterClockWiseInsideTest(unittest.TestCase):
     def test_matches_correctly(self):
-        val = counterclockwiseinside([1, 1],
-                                     [[0, 0], [2, 0], [3, 1], [2, 2], [0, 2]])
+        val = cr.counterclockwiseinside([1, 1], [[0, 0], [2, 0], [3, 1],
+                                                 [2, 2], [0, 2]])
         self.assertTrue(val)
 
     def test_rejects_correctly(self):
-        val = counterclockwiseinside([6, 1],
-                                     [[0, 0], [2, 0], [3, 1], [2, 2], [0, 2]])
+        val = cr.counterclockwiseinside([6, 1], [[0, 0], [2, 0], [3, 1],
+                                                 [2, 2], [0, 2]])
         self.assertFalse(val)
