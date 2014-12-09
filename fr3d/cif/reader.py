Fix computation of symmetry operated coordinates
I was computing these operations incorrectly because I did not use the
transformation vector. I'm now using it and am hopefully making the correct
transformations. I'm using the values provided by Dr. Zirbel as tests for 4NGG
though this makes it seem like my values for 1WMQ were wrong. This is possible
and likely so I've changed the expected values.

fr3d/cif/reader.py
@@ -229,8 +229,10 @@ class Cif(object):
     def __apply_symmetry__(self, atom, symmetry):
         coords = [float(atom['Cartn_x']),
                   float(atom['Cartn_y']),
-                  float(atom['Cartn_z'])]
-        return np.array(coords) # np.cross(symmetry['transform'], np.array(coords))[0:3]
+                  float(atom['Cartn_z']),
+                  1.0]
+        result = np.dot(symmetry['transform'], np.array(coords))
+        return result[0:3].T
 
     def table(self, name):
         return Table(self, self.__block__(name))

tests/cif/high_level_tests.py
@@ -169,13 +169,32 @@ class StructureWithSymmetry(ReaderTest):
 
     def test_can_compute_correct_transformed_coordinates(self):
         val = self.data[0].centers['N']
-        ans = np.array([-22.297, -17.982, 74.534])
+        ans = np.array([26.721, -10.319, 74.534])
         np.testing.assert_array_almost_equal(ans, val, decimal=3)
 
     def test_can_compute_correct_standard_coordinates(self):
         val = self.data[2].centers['N']
         ans = np.array([-4.424, 28.301, 74.534])
         np.testing.assert_array_almost_equal(ans, val)
+
+
+class StructureWithTransformationMatrix(ReaderTest):
+    name = '4NGG'
+
+    def setUp(self):
+        super(StructureWithTransformationMatrix, self).setUp()
+        self.data = list(self.structure.atoms())
+        self.data = list(self.structure.residues(chain='B', number=1))
+
+    def test_can_compute_correct_transformed_coordinates(self):
+        val = self.data[0].centers["C4'"]
+        ans = np.array([-19.794, 41.314, 11.949])
+        np.testing.assert_array_almost_equal(ans, val, decimal=3)
+
+    def test_can_compute_standard_coordinates(self):
+        val = self.data[1].centers["C4'"]
+        ans = np.array([-19.794, 55.926, -11.949])
+        np.testing.assert_array_almost_equal(ans, val, decimal=3)
 
 
 class AtomTest(ReaderTest):