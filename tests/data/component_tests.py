a0be97ce1578f64ee6bb3d0c06ae1ce01bd97a80
Blake Sweeney <blakes.85@gmail.com>
2015-07-14 13:30:42 -0400

    Add tests for atoms_within

--- tests/data/component_tests.py
+++ tests/data/component_tests.py
@@ -1,4 +1,5 @@
 import unittest as ut
+from nose import SkipTest
 
 import numpy as np
 
@@ -152,75 +153,75 @@ class TransformTest(ut.TestCase):
 class InferHydrogenTest(ut.TestCase):
     def setUp(self):
         some_atoms = [
-            Atom(insertion_code='?', component_id='G', name='P', symmetry='1_555',
-                 component_number='118', chain='B', y=54.015, x=47.242,
-                 model='1', z=51.393, pdb='1GID'),
-            Atom(insertion_code='?', component_id='G', name='OP1', symmetry='1_555',
-                 component_number='118', chain='B', y=53.619, x=45.943,
-                 model='1', z=50.812, pdb='1GID'),
-            Atom(insertion_code='?', component_id='G', name='OP2', symmetry='1_555',
-                 component_number='118', chain='B', y=55.016, x=48.096,
-                 model='1', z=50.668, pdb='1GID'),
-            Atom(insertion_code='?', component_id='G', name="O5'", symmetry='1_555',
-                 component_number='118', chain='B', y=54.52, x=47.009,
-                 model='1', z=52.887, pdb='1GID'),
-            Atom(insertion_code='?', component_id='G', name="C5'", symmetry='1_555',
-                 component_number='118', chain='B', y=53.848, x=46.12,
-                 model='1', z=53.764, pdb='1GID'),
-            Atom(insertion_code='?', component_id='G', name="C4'", symmetry='1_555',
-                 component_number='118', chain='B', y=54.529, x=46.123,
-                 model='1', z=55.11, pdb='1GID'),
-            Atom(insertion_code='?', component_id='G', name="O4'", symmetry='1_555',
-                 component_number='118', chain='B', y=54.338, x=47.426,
-                 model='1', z=55.73, pdb='1GID'),
-            Atom(insertion_code='?', component_id='G', name="C3'", symmetry='1_555',
-                 component_number='118', chain='B', y=56.037, x=45.94,
-                 model='1', z=55.051, pdb='1GID'),
-            Atom(insertion_code='?', component_id='G', name="O3'", symmetry='1_555',
-                 component_number='118', chain='B', y=56.374, x=44.553,
-                 model='1', z=54.993, pdb='1GID'),
-            Atom(insertion_code='?', component_id='G', name="C2'", symmetry='1_555',
-                 component_number='118', chain='B', y=56.495, x=46.647,
-                 model='1', z=56.326, pdb='1GID'),
-            Atom(insertion_code='?', component_id='G', name="O2'", symmetry='1_555',
-                 component_number='118', chain='B', y=56.349, x=45.878,
-                 model='1', z=57.494, pdb='1GID'),
-            Atom(insertion_code='?', component_id='G', name="C1'", symmetry='1_555',
-                 component_number='118', chain='B', y=55.528, x=47.827,
-                 model='1', z=56.387, pdb='1GID'),
-            Atom(insertion_code='?', component_id='G', name='N9', symmetry='1_555',
-                 component_number='118', chain='B', y=56.026, x=49.033,
-                 model='1', z=55.738, pdb='1GID'),
-            Atom(insertion_code='?', component_id='G', name='C8', symmetry='1_555',
-                 component_number='118', chain='B', y=55.785, x=49.442,
-                 model='1', z=54.455, pdb='1GID'),
-            Atom(insertion_code='?', component_id='G', name='N7', symmetry='1_555',
-                 component_number='118', chain='B', y=56.371, x=50.566,
-                 model='1', z=54.161, pdb='1GID'),
-            Atom(insertion_code='?', component_id='G', name='C5', symmetry='1_555',
-                 component_number='118', chain='B', y=57.031, x=50.913,
-                 model='1', z=55.323, pdb='1GID'),
-            Atom(insertion_code='?', component_id='G', name='C6', symmetry='1_555',
-                 component_number='118', chain='B', y=57.827, x=52.019,
-                 model='1', z=55.608, pdb='1GID'),
-            Atom(insertion_code='?', component_id='G', name='O6', symmetry='1_555',
-                 component_number='118', chain='B', y=58.131, x=52.969,
-                 model='1', z=54.873, pdb='1GID'),
-            Atom(insertion_code='?', component_id='G', name='N1', symmetry='1_555',
-                 component_number='118', chain='B', y=58.301, x=51.975,
-                 model='1', z=56.907, pdb='1GID'),
-            Atom(insertion_code='?', component_id='G', name='C2', symmetry='1_555',
-                 component_number='118', chain='B', y=58.033, x=50.991,
-                 model='1', z=57.814, pdb='1GID'),
-            Atom(insertion_code='?', component_id='G', name='N2', symmetry='1_555',
-                 component_number='118', chain='B', y=58.557, x=51.138,
-                 model='1', z=59.032, pdb='1GID'),
-            Atom(insertion_code='?', component_id='G', name='N3', symmetry='1_555',
-                 component_number='118', chain='B', y=57.299, x=49.949,
-                 model='1', z=57.556, pdb='1GID'),
-            Atom(insertion_code='?', component_id='G', name='C4', symmetry='1_555',
-                 component_number='118', chain='B', y=56.828, x=49.974,
-                 model='1', z=56.301, pdb='1GID')
+            Atom(insertion_code='?', component_id='G', name='P',
+                 symmetry='1_555', component_number='118', chain='B', y=54.015,
+                 x=47.242, model='1', z=51.393, pdb='1GID'),
+            Atom(insertion_code='?', component_id='G', name='OP1',
+                 symmetry='1_555', component_number='118', chain='B', y=53.619,
+                 x=45.943, model='1', z=50.812, pdb='1GID'),
+            Atom(insertion_code='?', component_id='G', name='OP2',
+                 symmetry='1_555', component_number='118', chain='B', y=55.016,
+                 x=48.096, model='1', z=50.668, pdb='1GID'),
+            Atom(insertion_code='?', component_id='G', name="O5'",
+                 symmetry='1_555', component_number='118', chain='B', y=54.52,
+                 x=47.009, model='1', z=52.887, pdb='1GID'),
+            Atom(insertion_code='?', component_id='G', name="C5'",
+                 symmetry='1_555', component_number='118', chain='B', y=53.848,
+                 x=46.12, model='1', z=53.764, pdb='1GID'),
+            Atom(insertion_code='?', component_id='G', name="C4'",
+                 symmetry='1_555', component_number='118', chain='B', y=54.529,
+                 x=46.123, model='1', z=55.11, pdb='1GID'),
+            Atom(insertion_code='?', component_id='G', name="O4'",
+                 symmetry='1_555', component_number='118', chain='B', y=54.338,
+                 x=47.426, model='1', z=55.73, pdb='1GID'),
+            Atom(insertion_code='?', component_id='G', name="C3'",
+                 symmetry='1_555', component_number='118', chain='B', y=56.037,
+                 x=45.94, model='1', z=55.051, pdb='1GID'),
+            Atom(insertion_code='?', component_id='G', name="O3'",
+                 symmetry='1_555', component_number='118', chain='B', y=56.374,
+                 x=44.553, model='1', z=54.993, pdb='1GID'),
+            Atom(insertion_code='?', component_id='G', name="C2'",
+                 symmetry='1_555', component_number='118', chain='B', y=56.495,
+                 x=46.647, model='1', z=56.326, pdb='1GID'),
+            Atom(insertion_code='?', component_id='G', name="O2'",
+                 symmetry='1_555', component_number='118', chain='B', y=56.349,
+                 x=45.878, model='1', z=57.494, pdb='1GID'),
+            Atom(insertion_code='?', component_id='G', name="C1'",
+                 symmetry='1_555', component_number='118', chain='B', y=55.528,
+                 x=47.827, model='1', z=56.387, pdb='1GID'),
+            Atom(insertion_code='?', component_id='G', name='N9',
+                 symmetry='1_555', component_number='118', chain='B', y=56.026,
+                 x=49.033, model='1', z=55.738, pdb='1GID'),
+            Atom(insertion_code='?', component_id='G', name='C8',
+                 symmetry='1_555', component_number='118', chain='B', y=55.785,
+                 x=49.442, model='1', z=54.455, pdb='1GID'),
+            Atom(insertion_code='?', component_id='G', name='N7',
+                 symmetry='1_555', component_number='118', chain='B', y=56.371,
+                 x=50.566, model='1', z=54.161, pdb='1GID'),
+            Atom(insertion_code='?', component_id='G', name='C5',
+                 symmetry='1_555', component_number='118', chain='B', y=57.031,
+                 x=50.913, model='1', z=55.323, pdb='1GID'),
+            Atom(insertion_code='?', component_id='G', name='C6',
+                 symmetry='1_555', component_number='118', chain='B', y=57.827,
+                 x=52.019, model='1', z=55.608, pdb='1GID'),
+            Atom(insertion_code='?', component_id='G', name='O6',
+                 symmetry='1_555', component_number='118', chain='B', y=58.131,
+                 x=52.969, model='1', z=54.873, pdb='1GID'),
+            Atom(insertion_code='?', component_id='G', name='N1',
+                 symmetry='1_555', component_number='118', chain='B', y=58.301,
+                 x=51.975, model='1', z=56.907, pdb='1GID'),
+            Atom(insertion_code='?', component_id='G', name='C2',
+                 symmetry='1_555', component_number='118', chain='B', y=58.033,
+                 x=50.991, model='1', z=57.814, pdb='1GID'),
+            Atom(insertion_code='?', component_id='G', name='N2',
+                 symmetry='1_555', component_number='118', chain='B', y=58.557,
+                 x=51.138, model='1', z=59.032, pdb='1GID'),
+            Atom(insertion_code='?', component_id='G', name='N3',
+                 symmetry='1_555', component_number='118', chain='B', y=57.299,
+                 x=49.949, model='1', z=57.556, pdb='1GID'),
+            Atom(insertion_code='?', component_id='G', name='C4',
+                 symmetry='1_555', component_number='118', chain='B', y=56.828,
+                 x=49.974, model='1', z=56.301, pdb='1GID')
         ]
 
         self.res = Component(some_atoms, type='rna', pdb='1GID', model=1,
@@ -235,3 +236,67 @@ class InferHydrogenTest(ut.TestCase):
         self.res.infer_hydrogens()
         atoms = list(self.res.atoms(name=['H1', 'H8', 'H9', '1H2', '2H2']))
         self.assertEqual(len(atoms), 5)
+
+    def test_infers_correct_location(self):
+        raise SkipTest()
+
+
+class AtomsWithin(ut.TestCase):
+    def setUp(self):
+        self.component1 = Component([
+            Atom(name='N9', x=3.0, y=3.0, z=3.0),
+            Atom(name='C4', x=2.0, y=2.0, z=2.0),
+            Atom(name='N3', x=1.0, y=1.0, z=1.0),
+            Atom(name='C3', x=0.0, y=0.0, z=0.0),
+        ])
+        self.component2 = Component([
+            Atom(name='N1', x=0.0, y=-1.0, z=0.0),
+            Atom(name='N3', x=-10.0, y=0.0, z=-1.0),
+            Atom(name='C2', x=-2.0, y=0.0, z=-3.0)
+        ])
+
+    def test_knows_if_an_atom_is_within(self):
+        val = self.component1.atoms_within(self.component2, cutoff=1.0)
+        self.assertTrue(val)
+
+    def test_works_with_negative_cutoff(self):
+        val = self.component1.atoms_within(self.component2, cutoff=-1.0)
+        self.assertTrue(val)
+
+    def test_knows_if_no_atoms_within_cutoff(self):
+        val = self.component1.atoms_within(self.component2, cutoff=0.5)
+        self.assertFalse(val)
+
+    def test_knows_if_no_atoms_within_negative_cutoff(self):
+        val = self.component1.atoms_within(self.component2, cutoff=-0.5)
+        self.assertFalse(val)
+
+    def test_can_use_given_list_for_first_atoms(self):
+        val = self.component1.atoms_within(self.component2, using=['C3', 'N3'],
+                                           cutoff=1.0)
+        self.assertTrue(val)
+
+    def test_knows_if_with_given_list_nothing_is_within(self):
+        val = self.component1.atoms_within(self.component2, using=['C4', 'N3'],
+                                           cutoff=1.0)
+        self.assertFalse(val)
+
+    def test_can_use_to_list_to_detect_near(self):
+        val = self.component1.atoms_within(self.component2, to=['N1', 'N3'],
+                                           cutoff=1.0)
+        self.assertTrue(val)
+
+    def test_can_use_to_list_to_detect_not_near(self):
+        val = self.component1.atoms_within(self.component2, to=['C2', 'N3'],
+                                           cutoff=1.0)
+        self.assertFalse(val)
+
+    def test_can_use_both_to_detect_near(self):
+        val = self.component1.atoms_within(self.component2, to=['N1', 'N3'],
+                                           using=['C3', 'N3'], cutoff=1.0)
+        self.assertTrue(val)
+
+    def test_can_use_both_to_detect_not_near(self):
+        val = self.component1.atoms_within(self.component2, to=['N1', 'N3'],
+                                           using=['C4', 'N3'], cutoff=1.0)
+        self.assertFalse(val)
