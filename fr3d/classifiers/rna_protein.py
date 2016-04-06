15cbc18520cfe28a331fbbdd3902f99a3799c64b
poorna11 <roy.poorna@gmail.com>
2016-04-06 14:29:50 -0400

    changed indentation
    removed syntactical errors

--- fr3d/classifiers/rna_protein.py
+++ fr3d/classifiers/rna_protein.py
@@ -12,17 +12,16 @@ class Classifier(BaseClassifier):
     def __init__(self):
         first = {'sequence': defs.RNAbaseheavyatoms.keys()}
         second = {'sequence': defs.aa_backconnect.keys()}
-        distance = {'use': 'center', 'cutoff': 4.0}
+        distance = {'use': 'center', 'cutoff': 10.0}
         super(Classifier, self).__init__(first=first, second=second,
                                          distance=distance)
 
     def classification(self, first, second):
         squared_xy_dist_list = []
         aa_z =[]
-
-         """Defines different sets of amino acids"""
-    stacked_aa = set (["TRP", "TYR", "PHE", "HIS", "ARG", "LYS", "ASN", "GLN", "LEU", "ILE", "PRO", "THR"])
-    pseudopair_aa = set (["ASP", "GLU", "ASN", "GLN", "HIS", "LYS", "ARG", "SER", "TYR", "TRP", "PHE", "VAL", "LEU", "ILE", "MET"])
+        """Defines different sets of amino acids"""
+        stacked_aa = set (["TRP", "TYR", "PHE", "HIS", "ARG", "LYS", "ASN", "GLN", "LEU", "ILE", "PRO", "THR"])
+        pseudopair_aa = set (["ASP", "GLU", "ASN", "GLN", "HIS", "LYS", "ARG", "SER", "TYR", "TRP", "PHE", "VAL", "LEU", "ILE", "MET"])
 
         for aa_atom in second.atoms(name=defs.aa_fg[second.sequence]):
             key = aa_atom.name
@@ -43,7 +42,7 @@ class Classifier(BaseClassifier):
                 return stacking_tilt(second)
             
         elif 3.1 < min(squared_xy_dist_list)< 35.2 and -2.0 <= mean_z < 2.0:
-            if second.sequence in pseudopair_aa::
+            if second.sequence in pseudopair_aa:
                 angle= compnt.angle_between_normals(first, second)
     
             if -1.3 <= angle <= 0.75 or 2.6 <= angle <= 3.14:
@@ -52,22 +51,22 @@ class Classifier(BaseClassifier):
                 return None
     
     
-def enough_HBs(base_residue, aa_residue, base_atoms):
-    """Calculates atom to atom distance of part "aa_part" of neighboring amino acids
+    def enough_HBs(self, aa_residue, base_atoms):
+        """Calculates atom to atom distance of part "aa_part" of neighboring amino acids
     of type "aa" from each atom of base. Only returns a pair of aa/nt if two
     or more atoms are within the cutoff distance"""
-    min_distance = 4
-    HB_atoms = set(['N', 'NH1','NH2','NE','NZ','ND1','NE2','O','OD1','OE1','OE2', 'OG', 'OH'])
-    n = 0
-    for base_atom in base_residue.atoms(name=base_atoms):
-        for aa_atom in aa_residue.atoms(name=aa_fg[aa_residue.sequence]):
+        min_distance = 4
+        HB_atoms = set(['N', 'NH1','NH2','NE','NZ','ND1','NE2','O','OD1','OE1','OE2', 'OG', 'OH'])
+        n = 0
+        for base_atom in self.atoms(name=base_atoms):
+            for aa_atom in aa_residue.atoms(name=defs.aa_fg[aa_residue.sequence]):
 
-            distance = np.subtract(base_atom.coordinates(), aa_atom.coordinates())
-            distance = np.linalg.norm(distance)
+                distance = np.subtract(base_atom.coordinates(), aa_atom.coordinates())
+                distance = np.linalg.norm(distance)
             if distance <= min_distance and aa_atom.name in HB_atoms:
                 n = n+1
-    if n>=2:
-        return True
+        return n>=2
+            
 
 def stacking_angle (base_residue, aa_residue):
     vec1 = compnt.normal_calculation(base_residue)
@@ -78,7 +77,7 @@ def stacking_angle (base_residue, aa_residue):
                 
     angle = angrot.angle_between_planes(vec1, vec2)
     
-    if aa_residue.sequence in set stacked_aa:
+    if aa_residue.sequence in stacked_aa:
         if angle <=0.67 or 2.43 <= angle <= 3.15:
             return "stacked"
         elif aa_residue.sequence in perpendicular_aa:
