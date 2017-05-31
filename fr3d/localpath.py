43e0e2dc32d377833fd1ba34ec9236f261c65e98
Craig L. Zirbel <zirbel@bgsu.edu>
2017-05-31 14:44:27 -0400

    Added local path definition file and changes to RNA-protein.py

---
+++ fr3d/localpath.py
@@ -0,0 +1,4 @@
+
+inputPath = 'C:\\Users\\zirbel\\Documents\\FR3D\\PDBFiles\\%s.cif'
+outputText = 'C:\\Users\\zirbel\\Documents\\FR3D\\RNAProtein\\proteinRNA_%s.txt'
+outputBaseAAFG = 'C:\\Users\\zirbel\\Documents\\FR3D\\RNAProtein\\aa-fg_base_%s.csv'
