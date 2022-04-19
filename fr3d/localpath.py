4faf05b9d7da98f303cd0aa54e8fab69393fc7fe
Craig L. Zirbel <zirbel@bgsu.edu>
2022-04-19 15:45:41 -0400

    Update localpath.py

--- fr3d/localpath.py
+++ fr3d/localpath.py
@@ -1,10 +1,12 @@
 # add variables specific to your machine; this file is not uploaded to GitHub
 # then reinstall fr3d-python by navigating to the fr3d-python folder and run "python setup.py install"
 
-inputPath = 'C:\\Users\\zirbel\\Documents\\FR3D\\PDBFiles\\%s.cif'
+inputPath = 'C:\\Users\\zirbel\\Documents\\FR3D\\PDBFiles\\'
+outputNAPairwiseInteractions = 'C:\\Users\\zirbel\\Documents\\FR3D\\NAPairwiseInteractions\\'
+
+
 outputText = 'C:\\Users\\zirbel\\Documents\\FR3D\\RNAProtein\\proteinRNA_%s.txt'
 outputBaseAAFG = 'C:\\Users\\zirbel\\Documents\\FR3D\\RNAProtein\\aa-fg_base_%s.csv'
-outputNAPairwiseInteractions = 'C:\\Users\\zirbel\\Documents\\FR3D\\NAPairwiseInteractions\\'
 outputNAPickleInteractions = 'C:\\Users\\zirbel\\Documents\\FR3D\\Python FR3D\\data\\pairs\\'
 contact_list_file = 'C:\\Users\\zirbel\\Documents\\FR3D\\RNAProtein\\contact_list_%s.txt'
 outputHTML = 'C:\\Users\\zirbel\\Documents\\FR3D\\RNAProtein\\output'
