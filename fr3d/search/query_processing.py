7dd13709407d4bafde9aa7c14892f37408540c5a
Craig L. Zirbel <zirbel@bgsu.edu>
2024-09-25 09:19:52 -0700

    Improve reading representative sets

--- fr3d/search/query_processing.py
+++ fr3d/search/query_processing.py
@@ -1175,9 +1175,21 @@ def calculateQueryConstraints(Q):
             continue
 
         # look up representative sets, if requested, and replace with IFE names
+        # Example: 	"https://rna.bgsu.edu/rna3dhub/nrlist/download/3.300/3.0A/csv"
+        # Example:  "https://rna.bgsu.edu/rna3dhub/nrlist/download/3.300/NMR/csv"
         if "nrlist" in search_file:           # referring to lists that are posted online
             listLoaded = False
             pathAndFileName = os.path.join(Q["DATAPATHUNITS"],'Representative_sets.pickle')
+
+            # make sure that search_file fits the pattern; avoid code injection
+            fields = search_file.split("/")
+            if not len(fields[6].split(".")) == 2 or not fields[6].split(".")[0].isdigit() or not fields[6].split(".")[1].isdigit():
+                fields[6] = 3.300
+                Q['errorMessage'].append("Invalid representative set %s, using 3.300" % fields[6])
+            if not fields[7] in ["1.5A","2.0A","2.5","3.0A","3.5A","4.0A","all","NMR"]:
+                fields[7] = "3.0A"
+                Q['errorMessage'].append("Invalid resolution %s, using 3.0A" % fields[7])
+            search_file = "https://rna.bgsu.edu/rna3dhub/nrlist/download/%s/%s/csv" % (fields[6],fields[7])
 
             # check in local file of representative sets first, in case already loaded
             if os.path.exists(pathAndFileName):
