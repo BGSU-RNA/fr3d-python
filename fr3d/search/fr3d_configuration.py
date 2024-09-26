a24325593bf04475190b1bc4b16c2ef42b34be39
Matlab Pipeline User <pipeline@rnaprod2.bgsu.edu>
2024-09-26 12:01:53 -0400

    Specify js location, not js code calls

--- fr3d/search/fr3d_configuration.py
+++ fr3d/search/fr3d_configuration.py
@@ -3,21 +3,17 @@ set paths and parameters for FR3D to use
 These can also be set in the query .json file
 """
 
-SERVER = False
-CIFPATH = "C:/Users/zirbel/Documents/FR3D/PDBFiles"
-DATAPATHUNITS = "C:/Users/zirbel/Documents/FR3D/Python FR3D/data/units"
-DATAPATHPAIRS = "C:/Users/zirbel/Documents/FR3D/Python FR3D/data/pairs"
-OUTPUTPATH = "C:/Users/zirbel/Documents/FR3D/Python FR3D/output/"
-JSONPATH = "C:/Users/zirbel/Documents/FR3D/Python FR3D/JSONqueries/"
-MAXTIME = 20
-MAXTIME = float('inf')
-MAXCANDIDATESHEATMAP = 300
-MAXCANDIDATES = 10000
-REFRESHTIME = 20
+SERVER = True
+DATAPATHUNITS = "/var/www/html/units"
+DATAPATHPAIRS = "/var/www/html/pairs"
+OUTPUTPATH    = "/var/www/fr3d/app/results"
+# CIFPATH       = "/usr/local/pipeline/hub-core/cif-files"  # we should not need this
+JSONPATH      = "/var/www/fr3d/app/results"  # where to look for JSON files by default
 
-# control where the output .html file looks for javascript files; optional.  Also awkward.
-JS1 = '  <script src="./js/JSmol.min.nojq.js"></script>'
-JS2 = '  <script src="./js/jquery.jmolTools.js"></script>'
-JS3 = '  <script src="./js/imagehandlinglocal.js"></script>'
-JS4 = '<script src="./js/jmolplugin.js" type="text/javascript"></script>'
-JS5 = '<script type="text/javascript" src="./js/heatmap.js"></script>'
+MAXTIME = float('inf')
+MAXTIME = 20
+MAXCANDIDATESHEATMAP = 300
+MAXCANDIDATES = 1000
+REFRESHTIME = 2
+
+JSLOCATION = './'  # js folder is in HTML folder
