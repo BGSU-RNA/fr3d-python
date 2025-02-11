acb388b935814f514be31e8b4500df9c994db0de
Craig L. Zirbel <zirbel@bgsu.edu>
2025-02-11 14:43:03 -0800

    Process NABIR structure list

--- fr3d/classifiers/develop_NA_pairwise_interactions.py
+++ fr3d/classifiers/develop_NA_pairwise_interactions.py
@@ -70,79 +70,6 @@ if args.worker:
 else:
     worker = 0
 
-PDB_list = ['5AJ3']
-PDB_list = ['6hiv']
-PDB_list = ['3QRQ','5J7L']
-PDB_list = ['4V9F','4YBB','4Y4O','6AZ3','4P95']
-PDB_list = ['3BT7']
-PDB_list = ['5I4A']
-PDB_list = ['6A2H']
-PDB_list = ['3JB9']
-PDB_list = ['1OCT']
-PDB_list = ['4v9fFH.pdb']
-PDB_list = ['5KCR', '4WOI', '6C4I', '5JC9', '5L3P', '5KPW', '3J9Y', '3J9Z', '6BU8', '5WF0', '4V55', '4V54', '4V57', '4V56', '4V50', '4V53', '4V52', '4WF1', '5H5U', '4V5B', '5WFS', '5O2R', '5WFK', '5LZD', '5LZA', '6O9J', '6O9K', '6ORL', '6ORE', '3R8O', '3R8N', '4V85', '5MDV', '5MDW', '4V80', '4U27', '4U26', '4U25', '4U24', '4U20', '5KPS', '6GXM', '5KPX', '4U1U', '3JBU', '4V9P', '3JBV', '6Q9A', '6DNC', '4U1V', '6GXO', '5IQR', '5NWY', '4V9C', '6OSK', '4V9D', '4V9O', '5MGP', '6Q97', '3JCJ', '5J91', '3JCD', '3JCE', '6I7V', '6GXN', '4V64', '5J7L', '5AFI', '6BY1', '6ENU', '4V7V', '4V7U', '4V7T', '4V7S', '3JA1', '6ENF', '6OUO', '6ENJ', '5JU8', '5J8A', '6GWT', '4YBB', '5NP6', '5J88', '5U9G', '5U9F', '4V6D', '4V6E', '4V6C', '5JTE', '6OT3', '5J5B', '4WWW', '6OSQ', '5U4J', '5MDZ', '5U4I', '6NQB', '5UYQ', '5UYP', '5MDY', '5WDT', '6H4N', '5UYK', '4V89', '5UYM', '5UYL', '5UYN', '5WE6', '5WE4', '5KCS', '4V4Q', '4V4H', '5IT8']
-PDB_list = ['4V51','4V9K']
-PDB_list = ['6WJR']
-PDB_list = ['6TPQ']
-PDB_list = ['4KTG']
-PDB_list = ['5KCR']
-PDB_list = ['7ECF']  # DNA quadruplex
-PDB_list = ['5J7L']
-PDB_list = ['4V9F','5J7L','4ARC']
-PDB_list = ['4ARC']
-PDB_list = ['4ARC']
-PDB_list = ['4V9F','6ZMI','7K00']
-PDB_list = ['2N1Q']
-PDB_list = ['203D']
-PDB_list = ['7k00']
-PDB_list = ['6CFJ']
-PDB_list = ['7K00']
-PDB_list = ['7MKY', '4JF2', '5VGW', '4ENC', '5XTM', '2R8S', '4PQV', '3RW6', '4BW0', '6CB3', '4K27', '5U3G', '7OF0', '4LVW', '5D5L', '2NZ4', '3NKB', '6TFG', '2Z75', '4YAZ', '5X2G', '4V9F', '7OZQ', '4Y4O', '4WFL', '1M5K', '7K16', '5FJC', '7O7Y', '6JQ5', '6S0Z', '3P22', '7OX9', '1Q96', '6KWQ', '3LQX', '6U8D', '6SVS', '3E5C', '7RQB', '2EZ6', '6DMC', '2V3C', '5M0I', '3MXH', '4YBB', '5B2P', '4P95', '7KKV', '3NPQ', '5DDP', '4NLF', '7P7Q', '6AZ3', '7D7W', '6S0X', '7RYG', '3AM1', '4PCJ', '5UZ6', '5B2T']
-PDB_list += ['5AH5', '4ENC', '7EOG', '1QU2', '2ZUE', '2QUW', '2QUS', '5KPY', '7OF0', '7EQJ', '1U0B', '5AOX', '3FOZ', '2DRA', '4YCO', '7C79', '4V9F', '4Y4O', '4WFL', '3RG5', '5UD5', '7K16', '7O7Y', '6S0Z', '4JXZ', '4J50', '3B31', '3ADD', '7RQB', '3OVB', '6UGG', '4PRF', '4YBB', '3VJR', '1QTQ', '7K98', '4P95', '2GDI', '7DCO', '7P7Q', '6AZ3', '4YYE', '6S0X', '5HR7', '7RYG', '3AM1', '2OEU', '3D2V', '1J1U']
-PDB_list = list(set(PDB_list))
-
-PDB_list = ['3AM1','4J50']  # these have symmetry operators, but no annotations there
-PDB_list = ['3AM1']  # these have symmetry operators, but no annotations there
-PDB_list = ['4J50']  # these have symmetry operators, but no annotations there
-PDB_list = ['4RKV','4J50','3AM1']
-PDB_list = ['4TNA.cif']
-PDB_list = ['5T2A']  # has a conflicting cBW annotation
-PDB_list = ['5UED']
-PDB_list = ['4V9F','6ZMI','7K00','4TNA']
-PDB_list = ['4TNA','7QI4']
-PDB_list = ['4K27']
-PDB_list = ['5B2R']
-PDB_list = ['1AJF', '1JTW', '1N66', '1R7W', '1S9S', '1U6P', '1ZIF', '1ZIG', '1ZIH', '2JXV', '2KPV', '2LK3', '2MXJ', '2MXL', '2N1Q', '4BY9', '6MCI', '6VU1', '6VVJ']
-PDB_list = ['2GDI']
-PDB_list = ['4J50']
-PDB_list = ['3RG5']  # had a problem annotating U|67 basepairs in Python 3.8
-PDB_list = ['4M6D']  # to see 4M6D|1|H|G|28  cSH  4M6D|1|H|U|29
-PDB_list = ['124D']  # RNA-DNA duplex
-PDB_list = ['1A1L']  # DNA-DNA duplex
-PDB_list = ['7JQQ']  # Five RNA chains and a DNA-DNA duplex
-PDB_list = ['7K00','8B0X']
-PDB_list = ['http://rna.bgsu.edu/rna3dhub/nrlist/download/3.280/2.5A/csv']
-PDB_list = ['http://rna.bgsu.edu/rna3dhub/nrlist/download/3.280/2.0A/csv']
-PDB_list = ['http://rna.bgsu.edu/rna3dhub/nrlist/download/3.280/1.5A/csv']
-PDB_list = ['1NBS','6PMO']
-PDB_list = ['6DVK']
-PDB_list = ['7QI4']
-PDB_list = ['7O7Y']
-PDB_list = ['1Q96']
-PDB_list = ['7QI4','6S0Z','4C8Z','5FQ5','5D5L','5AOX','7K16','5CCB','4V8Y','8D28','4AL5','7OZQ','6YL5','4NLF','7LO9','7QIW','7QIZ','4C8Y','5F4Q','8D28']  # have hydrogens that are named wrong
-PDB_list = ['7O7Y','7O7Z','8A3D']
-PDB_list = ['1L2X']
-PDB_list = ['7ZW0','6S0X','7UVZ']
-PDB_list = ['6XU8']
-PDB_list = ['http://rna.bgsu.edu/rna3dhub/nrlist/download/3.285/1.5A/csv']
-
-PDB_list = ['4TNA','5KFX','8B0X']
-PDB_list = ['4V9F']
-PDB_list = ['2IZN']   # gets an interaction called p_1??
-PDB_list = ['4TNA']
-PDB_list = ['7JQQ']  # Five RNA chains and a DNA-DNA duplex
-PDB_list = ['464D']
-PDB_list = ['8EA4']
 PDB_list = ['4V9F']
 
 from DNA_2A_list import PDB_list   # define PDB_list as a list of DNA structures
@@ -153,11 +80,19 @@ PDB_list = ['4V9F','6AZ3','6GYV','7O7Y','7OYC','7QI4','7QIW','7V9E','8A98','8AZW
 PDB_list = ['http://rna.bgsu.edu/rna3dhub/nrlist/download/NR/3.349/3.0A/csv','8B0X','8GLP','http://rna.bgsu.edu/rna3dhub/nrlist/download/NR/3.349/2.5A/csv','http://rna.bgsu.edu/rna3dhub/nrlist/download/NR/3.349/2.0A/csv','http://rna.bgsu.edu/rna3dhub/nrlist/download/NR/3.349/1.5A/csv']
 
 if True:
-    # read all structures from C:\Users\zirbel\Documents\FR3D\Python FR3D\data\pairs_datmos
-    import glob
-    datmos_list = glob.glob("C:/Users/zirbel/Documents/FR3D/Python FR3D/data/pairs_datmos/*.txt")
-    PDB_list = [os.path.basename(x).split('_')[0].upper() for x in datmos_list]
-    print('Using %d files from datmos' % len(PDB_list))
+    # read chains from datmos / nabir
+    PDB_set = set()
+    PDB_chain_set = set()
+    for mt in ['RNA','DNA']:
+        filename = '%s_reference_chains.csv' % mt
+        path_filename = os.path.join('C:/Users/zirbel/Documents/FR3D/Python FR3D/data/pairs_datmos',filename)
+        with open(path_filename,'rt') as f:
+            lines = f.readlines()
+        for line in lines:
+            pdb,chain,desc,count = line.split(",")
+            PDB_set.add(pdb.upper())
+            PDB_chain_set.add(pdb.upper()+"|1|"+chain)
+    PDB_list = list(PDB_set)
 
 # zzz
 
