08f88b832abf79fc9aaf6f7fc97f0775ca49e5ce
Craig L. Zirbel <zirbel@bgsu.edu>
2026-03-08 10:17:15 -0700

    Update folder location

--- fr3d/localpath.py
+++ fr3d/localpath.py
@@ -3,13 +3,9 @@
 
 inputPath = 'C:\\Users\\zirbel\\Documents\\PDBFiles\\'
 outputNAPairwiseInteractions = 'C:\\Users\\zirbel\\Documents\\NAPairwiseInteractions\\'
-
-outputText = 'C:\\Users\\zirbel\\Documents\\RNAProtein\\proteinRNA_%s.txt'
-outputBaseAAFG = 'C:\\Users\\zirbel\\Documents\\RNAProtein\\aa-fg_base_%s.csv'
-contact_list_file = 'C:\\Users\\zirbel\\Documents\\RNAProtein\\contact_list_%s.txt'
-outputHTML = 'C:\\Users\\zirbel\\Documents\\RNAProtein\\output'
+outputHTML = 'C:\\Users\\zirbel\\Documents\\PythonFR3D\\output'
 
 # the following directory will be for both units and pairs since that's what fr3d uses
 fr3d_pickle_path = 'C:\\Users\\zirbel\\Documents\\PythonFR3D\\data'
 
-storeMatlabFR3DPairs = "C:/Users/zirbel/Documents/FR3D/PythonFR3D/data/pairs/"
+storeMatlabFR3DPairs = "C:/Users/zirbel/Documents/PythonFR3D/data/pairs/"
